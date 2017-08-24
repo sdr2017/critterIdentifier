09dc078789710548b6ac32e3e606a5fd9ea71459
ShapeoftheClouds <amandagracebutt@gmail.com>
2017-08-23 21:26:20 -0600

    added express variables

--- models/spiders.js
+++ models/spiders.js
@@ -1,21 +1,18 @@
 module.exports = function(sequelize, DataTypes){
-	
-		
+
+var Spiders = sequelize.define("spider", {
 	UserId: DataTypes.Integer,
 	Identified: DataTypes.BOOLEAN,
-    Name: DataTypes.STRING,
-    Dangerous: DataTypes.BOOLEAN,
-    ZipCode: DataTypes.INTEGER,
+	Name: DataTypes.STRING,
+	Dangerous: DataTypes.BOOLEAN,
+	ZipCode: DataTypes.INTEGER,
 	Size: DataTypes.STRING,
-    Color: DataTypes.STRING,
-    Hairy: DataTypes.BOOLEAN,
-    Web: DataTypes.BOOLEAN	
-	},
-	
-	{paranoid:true})
-	
-	return Spiders;
-	
-	
-	
+	Color: DataTypes.STRING,
+	Hairy: DataTypes.BOOLEAN,
+	Web: DataTypes.BOOLEAN
+},
+
+{paranoid:true})
+
+return Spiders;
 }
