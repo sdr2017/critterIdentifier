3bd8b8b77f334ca41d27e528532aeb683d5b9fb2
ShapeoftheClouds <amandagracebutt@gmail.com>
2017-08-23 18:32:42 -0600

    added autocomplete for spiders

--- models/critters.js
+++ models/critters.js
@@ -1,1 +1,10 @@
 // JS goes here
+$( document ).ready(function() {
+  $( "#critterSearch" ).autocomplete({
+    source: [ "brown recluse", "black widow", "hobo spider", "funnel web grass spider", "mouse spider",
+  "wolf spider", "orb weaving spider", "cellar spider", "huntsman spider", "tarantula", "web spinners", "orb weaver",
+"black and yellow spiders", "argiope", "jewel spider", "orchard spider", "nursery web spider", "trapdoor spider",
+"fishing spider", "jumping spider", "flower spider", "parson spider", "green lynx spider", "daddy long legs"  ]
+  });
+  console.log("Why aren't you working!");
+});
--- models/index.js
+++ models/index.js
@@ -1,1 +1,9 @@
 // JS goes here
+
+var fs = require('fs');
+var path = require('path');
+var Sequelize = require('sequelize');
+var basename = path.basename(module.filename);
+var env = process.env.NODE_ENV || 'development';
+var config = (__dirname + '/../config/config.json')[env];
+var db = {};
