df023671fd674af0a1d70e879d7e39f16decf9de
ctacderas <ctacderas@gmail.com>
2017-08-26 11:28:27 -0600

    Fixed HTML routing

--- routing/html-routes.js
+++ routing/html-routes.js
@@ -16,23 +16,15 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, "../views/index.html"));
   });
 
-  // The Category route loads cms.html
-  app.get("/category.html", function(req, res) {
+  // The Category route loads Category.html
+  app.get("/category", function(req, res) {
     res.sendFile(path.join(__dirname, "../views/category.html"));
   });
 
   // The Upload route loads upload.html
-<<<<<<< HEAD
-<<<<<<< HEAD
+
   app.get("/upload", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/upload.html"));
-=======
-  app.get("/blog", function(req, res) {
-=======
-  app.get("/upload.html", function(req, res) {
->>>>>>> 70116f2cad7537f542e8f626d4368d2fb69638f8
     res.sendFile(path.join(__dirname, "../views/upload.html"));
->>>>>>> e35045799bebb772fabb3f6e052d2a5157456ce5
   });
 
 };
