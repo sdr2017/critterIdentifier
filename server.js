0607363fcac86c53a520f855b27d5f120ff9fb69
Sarah Diaz-Rivera <sarahjeanlinnebur@yahoo.com>
2017-08-25 19:59:10 -0600

    adding javascript back into category.html so modal works

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ var PORT = process.env.PORT || 8080;
 var app = express();
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
+debugger;
 
 db.sequelize.sync().then(function(){
 
