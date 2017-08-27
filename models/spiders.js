ae62d0835bcf86487e6e539c281781ef6a0f44aa
Kirk Henderson <kirk733@me.com>
2017-08-27 11:57:13 -0600

    minor adj to spider model

--- models/spiders.js
+++ models/spiders.js
@@ -36,11 +36,9 @@ module.exports = function(sequelize, DataTypes){
 
 
 	Spider.associate = function(models) {
-    // We're saying that a Post should belong to an Author
-    // A Post can't be created without an Author due to the foreign key constraint
-    Spider.belongsTo(models.user, {
-      foreignKey: {
-        allowNull: false
+    	Spider.belongsTo(models.user, {
+			foreignKey: {
+			allowNull: false
       }
     });
   };
