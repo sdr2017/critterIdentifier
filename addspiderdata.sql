7dabb5d87c86af238b8ee1c61a33db6d33decbc5
Kirk Henderson <kirk733@me.com>
2017-08-27 12:46:24 -0600

    update sql

--- addspiderdata.sql
+++ addspiderdata.sql
@@ -1,4 +1,5 @@
 -- Creates new rows containing data in all named columns --
+USE critters;
 
 INSERT INTO critters.users (`id`,`email`,`createdAt`,`updatedAt`) VALUES (DEFAULT,'booboo@bar.com','2017-08-27 13:40:17','2017-08-27 13:40:17');
 
@@ -18,4 +19,3 @@ INSERT INTO `spiders` (`id`,`identified`,`name`,`dangerous`,`zipCode`,`size`,`co
 INSERT INTO critters.users (`id`,`email`,`createdAt`,`updatedAt`) VALUES (DEFAULT,'margarita@bar.com','2017-08-27 13:40:17','2017-08-27 13:40:17');
 
 INSERT INTO `spiders` (`id`,`identified`,`name`,`dangerous`,`zipCode`,`size`,`color`,`hairy`,`web`,`link`,`createdAt`,`updatedAt`,`userId`) VALUES (DEFAULT,false,'banded spider',true,80303,'Large','black',true,true,'https://s3-us-west-2.amazonaws.com/critterbucket/1503796764234bandedspider.jpg','2017-08-27 13:40:17','2017-08-27 13:40:17',5);
-
