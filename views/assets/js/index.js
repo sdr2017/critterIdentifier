7bcdf406025c26a260a5d18d40a4f62e3cce4487
ctacderas <ctacderas@gmail.com>
2017-08-27 14:40:21 -0600

    Connected Index.JS file

--- views/assets/js/index.js
+++ views/assets/js/index.js
@@ -1,4 +1,15 @@
 $(document).ready(function() {
-
-
+	var dir = "../../../uploads";
+	var fileextension = ".jpg";
+	$.ajax({
+	    //This will retrieve the contents of the folder
+	    url: dir,
+	    success: function (data) {
+	        //List all .jpg file names in the page
+	        $(data).find("a:contains(" + fileextension + ")").each(function () {
+	            // var filename = this.href.replace(window.location.host, "").replace("http://", "");
+	            $("body").append("<img src='" + dir + filename + "'>");
+	        });
+	    }
+	});
 });
