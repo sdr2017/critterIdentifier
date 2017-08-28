b811908fdd0d8765511d1b020575e955e447d57e
Sarah Diaz-Rivera <sarahjeanlinnebur@yahoo.com>
2017-08-27 22:34:10 -0600

    category image styling and beginning parallax scroll category.html

--- views/assets/js/category.js
+++ views/assets/js/category.js
@@ -9,3 +9,19 @@ $(document).ready(function() {
       	event.preventDefault();
       	$(".commentText").text(".userComment").val();
       });
+
+
+
+/* Scroll event handler */
+    $(window).bind('scroll',function(e){
+    	parallaxScroll();
+    });
+    
+
+/* Parallax Scroll */
+function parallaxScroll(){
+	var scrolled = $(window).scrollTop();
+	$('#parallax-3').css('top',(0-(scrolled*.25))+'px');
+	$('#parallax-2').css('top',(0-(scrolled*.5))+'px');
+	$('#parallax-1').css('top',(0-(scrolled*.75))+'px');
+}
