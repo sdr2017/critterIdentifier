cfddfbd737dc81928344a1d4807c1431c1a3906e
Dustin J Siegel <weenzombie@gmail.com>
2017-09-02 21:08:30 -0600

    updated the format of the modals files

--- models/spiders.js
+++ models/spiders.js
@@ -1,45 +1,46 @@
-module.exports = function(sequelize, DataTypes){
+// Uses sequelize, file saver and path to connect our spiders models to the database
+module.exports = function(sequelize, DataTypes) {
 
-	var Spider = sequelize.define("Spider", {
+    var Spider = sequelize.define("Spider", {
 
-	//UserId: DataTypes.INTEGER,
+        //UserId: DataTypes.INTEGER,
 
-	identified: {
-    type: DataTypes.BOOLEAN,
-    default: false
-  },
+        identified: {
+            type: DataTypes.BOOLEAN,
+            default: false
+        },
 
-  name: DataTypes.STRING,
+        name: DataTypes.STRING,
 
-  dangerous: {
-    type: DataTypes.BOOLEAN,
-    default: false
-  },
+        dangerous: {
+            type: DataTypes.BOOLEAN,
+            default: false
+        },
 
-  zipCode: DataTypes.INTEGER,
-  size: DataTypes.STRING,
-  color: DataTypes.STRING,
-  hairy: {
-    type: DataTypes.BOOLEAN,
-    default: false
-  },
-  web: {
-    type: DataTypes.BOOLEAN,
-    default: false
-  },
-  comment: {
-    type: DataTypes.STRING,
-  },
-  link: DataTypes.STRING,
+        zipCode: DataTypes.INTEGER,
+        size: DataTypes.STRING,
+        color: DataTypes.STRING,
+        hairy: {
+            type: DataTypes.BOOLEAN,
+            default: false
+        },
+        web: {
+            type: DataTypes.BOOLEAN,
+            default: false
+        },
+        comment: {
+            type: DataTypes.STRING,
+        },
+        link: DataTypes.STRING,
 
-}, { paranoid:true });
+    }, { paranoid: true });
 
-	Spider.associate = function(models) {
-    	Spider.belongsTo(models.User, {
-			foreignKey: {
-			allowNull: false
-      }
-    });
-  };
-  return Spider;
+    Spider.associate = function(models) {
+        Spider.belongsTo(models.User, {
+            foreignKey: {
+                allowNull: false
+            }
+        });
+    };
+    return Spider;
 };
