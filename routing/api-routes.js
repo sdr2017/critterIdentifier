f808e5b625c35e03e0d0502d8a9dc2e33d5a89a0
Dustin J Siegel <weenzombie@gmail.com>
2017-09-02 20:45:38 -0600

    updated the format of the routes files

--- routing/api-routes.js
+++ routing/api-routes.js
@@ -11,69 +11,70 @@ var db = require("../models");
 // =============================================================
 module.exports = function(app) {
 
-  app.get("/api/users", function(req, res) {
-    // Here we add an "include" property to our options in our findAll query
-    // We set the value to an array of the models we want to include in a left outer join
-    // In this case, just db.Post
-    db.User.findAll({
-      include: [db.Spider]
-    }).then(function(dbUser) {
-      res.json(dbUser);
+    app.get("/api/users", function(req, res) {
+        // Here we add an "include" property to our options in our findAll query
+        // We set the value to an array of the models we want to include in a left outer join
+        // In this case, just db.Post
+        db.User.findAll({
+            include: [db.Spider]
+        }).then(function(dbUser) {
+            res.json(dbUser);
+        });
     });
-  });
 
-  app.get("/api/users/:id", function(req, res) {
-    db.User.findOne({
-      where: {
-        id: req.params.id
-      },
-      include: [db.Spider]
-    }).then(function(dbUser) {
-      res.json(dbUser);
+    app.get("/api/users/:id", function(req, res) {
+        db.User.findOne({
+            where: {
+                id: req.params.id
+            },
+            include: [db.Spider]
+        }).then(function(dbUser) {
+            res.json(dbUser);
+        });
     });
-  });
 
-  app.post("/api/users", function(req, res) {
+    app.post("/api/users", function(req, res) {
 
-    db.User.create(req.body).then(function(dbUser) {
-      res.json(dbUser);
+        db.User.create(req.body).then(function(dbUser) {
+            res.json(dbUser);
+        });
     });
-  });
 
-
-  // GET route for getting all of the posts
-  app.get("/api/spiders", function(req, res) {
-    var query = {};
-    if (req.query.userid) {
-      query.userid = req.query.userid;
-    }
-    // Here we add an "include" property to our options in our findAll query
-    // We set the value to an array of the models we want to include in a left outer join
-    // In this case, just db.Author
-    db.Spider.findAll({
-      where: query,
-      order: [['id', 'ASC']],
-      include: [db.User]
-    }).then(function(dbSpider) {
-      res.json(dbSpider);
+    // GET route for getting all of the posts
+    app.get("/api/spiders", function(req, res) {
+        var query = {};
+        if (req.query.userid) {
+            query.userid = req.query.userid;
+        }
+        // Here we add an "include" property to our options in our findAll query
+        // We set the value to an array of the models we want to include in a left outer join
+        // In this case, just dbSpider
+        db.Spider.findAll({
+            where: query,
+            order: [
+                ['id', 'ASC']
+            ],
+            include: [db.User]
+        }).then(function(dbSpider) {
+            res.json(dbSpider);
+        });
     });
-  });
 
-  app.get("/api/spiders/:id", function(req, res) {
-    db.Spider.findOne({
-      where: {
-        id: req.params.id
-      },
-      include: [db.User]
-    }).then(function(dbSpider) {
-      res.json(dbSpider);
+    app.get("/api/spiders/:id", function(req, res) {
+        db.Spider.findOne({
+            where: {
+                id: req.params.id
+            },
+            include: [db.User]
+        }).then(function(dbSpider) {
+            res.json(dbSpider);
+        });
     });
-  });
 
-  app.post("api/spiders", function(req, res) {
-    db.Spider.create(req.body).then(function(dbSpider) {
-      res.json(dbSpider);
-  });
-});
+    app.post("api/spiders", function(req, res) {
+        db.Spider.create(req.body).then(function(dbSpider) {
+            res.json(dbSpider);
+        });
+    });
 
 };
